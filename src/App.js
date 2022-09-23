ddfbbe521f4db941589c5899d105ed912350519d
Rhea Dieberger <rdieberger@gmail.com>
2022-09-23 17:33:15 +0200

    Implement basic functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import { useEffect, useState } from 'react';
 
 function App() {
-  // Define Guest List API url
+  // Definitions
   const baseUrl =
-    'https://28f51e24-1fa9-4952-9b77-09213d1f0356.id.repl.co/guests/';
+    'https://28f51e24-1fa9-4952-9b77-09213d1f0356.id.repl.co/guests';
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [checkBoxValue, setCheckBoxValue] = useState([]);
+  const [guests, setGuests] = useState([]);
+  // const [attendingState, setAttendingState] = useState([]);
 
-  const [firstName, setFirstName] = useState([]);
-  const [lastName, setLastName] = useState([]);
+  // Get all guests
+  async function getAllGuests() {
+    const response = await fetch(baseUrl);
+    const allGuests = await response.json();
+    setGuests(allGuests);
+  }
+
+  useEffect(() => {
+    getAllGuests().catch(() => {});
+  }, []);
+
+  // Delete a guest from the guest list
+  async function deleteGuest(id) {
+    const response = await fetch(`${baseUrl}/${id}`, {
+      method: 'DELETE',
+    });
+    const deletedGuest = await response.json();
+    console.log(deletedGuest);
+  }
 
   // Store new guest in API
   async function createGuest() {
@@ -14,17 +36,20 @@ function App() {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        firstName: 'Rhea',
-        lastName: 'dieberger',
+        firstName: firstName,
+        lastName: lastName,
       }),
     });
     const data = await response.json();
     console.log(data);
+    setFirstName('');
+    setLastName('');
+    await getAllGuests();
   }
 
-  // Update a guests status from 'not attending' to 'attending'
-  async function updateGuest() {
-    const response = await fetch(`${baseUrl}2`, {
+  // Update a guests status from 'not attending' to 'attending' on checkbox change (checked = attending, unchecked = not attending)
+  async function updateGuest(id) {
+    const response = await fetch(`${baseUrl}/${id}`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
@@ -33,59 +58,126 @@ function App() {
     });
     const updatedGuest = await response.json();
     console.log(updatedGuest);
+    await getAllGuests();
   }
 
-  // Delete a guest completely from the guest list
-  async function deleteGuest() {
-    const response = await fetch(`${baseUrl}1`, { method: 'DELETE' });
-    const deletedGuest = await response.json();
-    console.log(deletedGuest);
+  async function unUpdateGuest(id) {
+    const response = await fetch(`${baseUrl}/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ attending: false }),
+    });
+    const updatedGuest = await response.json();
+    console.log(updatedGuest);
+    await getAllGuests();
   }
 
   return (
     <div>
       <h1>Guest List</h1>
-      <input
-        aria-label="First Name"
-        value={firstName}
-        onChange={(event) => {
-          setFirstName(event.currentTarget.value);
-        }}
-        onKeyPress={(event) => {
-          event.key === 'Enter' && console.log(firstName);
-        }}
-      />
-      <br />
-      <br />
-      <input
-        aria-label="Last Name"
-        value={lastName}
-        onChange={(event) => {
-          setLastName(event.currentTarget.value);
-        }}
-        onKeyPress={(event) => {
-          event.key === 'Enter' && console.log(lastName);
-        }}
-      />
-      <br />
-      <br />
-      <button
-        onClick={() => {
-          updateGuest().catch(() => {});
+      {/* Form for guest input */}
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
         }}
       >
-        Send
-      </button>
+        <input
+          aria-label="First Name"
+          value={firstName}
+          onChange={(event) => {
+            setFirstName(event.currentTarget.value);
+          }}
+        />
+        <br />
+        <br />
+        <input
+          aria-label="Last Name"
+          value={lastName}
+          onChange={(event) => {
+            setLastName(event.currentTarget.value);
+          }}
+          onKeyPress={async (event) =>
+            event.key === 'Enter' ? await createGuest() : null
+          }
+        />
+        <br />
+        <p>Press enter to submit your guest</p>
+      </form>
+      <br />
+      <br />
+      <br />
       <table>
-        <tr>
-          <th>First Name</th>
-          <br />
-          <br />
-          <th>Last Name</th>
-          <br />
-          <br />
-          <th>Status of Attendance</th>
-        </tr>
+        {/* Head of the Guest List */}
+        <thead>
+          <tr>
+            <th>First & Last Name</th>
+            <th>Status of Attendance </th>
+            <th>Removal</th>
+          </tr>
+          {/* Guests from API */}
+          {guests.map((guest) => {
+            return (
+              <tr key={guest.id}>
+                <td>
+                  {guest.firstName} {guest.lastName}
+                </td>
+                <td>
+                  <input
+                    checked={guest.attending}
+                    type="checkbox"
+                    onChange={async (event) => {
+                      const newState = event.currentTarget.checked;
+                      if (newState === true) {
+                        await updateGuest(guest.id);
+                      } else {
+                        await unUpdateGuest(guest.id);
+                      }
+                    }}
+                  />
+                </td>
+                <td>
+                  <button
+                    onClick={async () => {
+                      await deleteGuest(guest.id);
+                      await getAllGuests();
+                      console.log(`${baseUrl}/${guest.id}`);
+                    }}
+                  >
+                    Remove
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+
+          {/* <tr key={newGuest.id}>
+            <td>
+              {newGuest.firstName} {newGuest.lastName}
+            </td>
+            <td>
+              {attendingState}
+              <input
+                checked={checkBoxValue}
+                type="checkbox"
+                onChange={async (event) => {
+                  const checked = event.currentTarget.checked;
+                  setCheckBoxValue(checked);
+                  if (checked) {
+                    setAttendingState('attending');
+                    await updateGuest();
+                  } else {
+                    setAttendingState('not attending');
+                  }
+                }}
+              />
+            </td>
+            <td>
+              <button>Remove</button>
+            </td>
+          </tr> */}
+        </thead>
       </table>
     </div>
   );
